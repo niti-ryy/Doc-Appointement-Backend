26e903771567d5b8177ac2137aa3d3fc86258fc4
saksham <phantomdx316@gmail.com>
2024-01-11 23:02:15 +0530

    minor changes in the counsellor model

--- Doc-Appointment/models/counselorModel.js
+++ Doc-Appointment/models/counselorModel.js
@@ -4,7 +4,7 @@ const counselorSchema = new mongoose.Schema({
   profile: { type: mongoose.Schema.Types.ObjectId, ref: 'Profile', unique: true },
   specialization: String,
   availability: [String],
-  experiences: String,
+  experience: Number,
   appointments: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Appointment' }],
   introductoryVideos: [String],
   patients: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
@@ -24,6 +24,9 @@ const counselorSchema = new mongoose.Schema({
     type:String,
     enum:["English","Hindi"],
     required:true
+  },
+  achievements:{
+    type:String
   }
 });
 
