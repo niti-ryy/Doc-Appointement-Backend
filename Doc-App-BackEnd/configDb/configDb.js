d88a8df6b4c58233cb472ee6578c84b7ee28b6c5
Saksham <saksham@pearlorganisation.com>
2024-02-06 12:51:02 +0530

    minor changes in the counselor backend

--- Doc-App-BackEnd/configDb/configDb.js
+++ Doc-App-BackEnd/configDb/configDb.js
@@ -1,12 +1,15 @@
-const mongoose=require("mongoose")
+const mongoose = require("mongoose");
 
-const configDb=async()=>{
-    try{
-        const db=await mongoose.connect("mongodb://127.0.0.1:27017/doc-appointment-app")
-        console.log("db connected successfully")
-    }catch(e){
-        console.log("failed to connected to db")
-    }   
-}
+let mongoURL =
+  process.env.MONGO_DB_URL || "mongodb://127.0.0.1:27017/doc-appointment-app";
 
-module.exports=configDb
+const configDb = async () => {
+  try {
+    const db = await mongoose.connect(mongoURL);
+    console.log("db connected successfully");
+  } catch (e) {
+    console.log("failed to connected to db", e.message);
+  }
+};
+
+module.exports = configDb;
