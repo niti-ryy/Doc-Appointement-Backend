e26c1438b436cf232d183764c9e864807d704255
Nitish <nitishoffica3@gmail.com>
2024-02-26 22:32:52 +0530

    added auth to all routes

--- Doc-App-BackEnd/routes/BlogsRoutes.js
+++ Doc-App-BackEnd/routes/BlogsRoutes.js
@@ -1,16 +1,34 @@
+// const express=require("express")
+// const blogsCltr = require("../controllers/blogsCltr")
+// const uploadImg = require("../middlewears/uploadImg")
+// const blogRouter=express.Router()
+// const {checkSchema}=require("express-validator")
+// const blogValidationSchema = require("../helpers/blog-validation")
+
+// blogRouter.route("/createBlog")
+//     .post(checkSchema(blogValidationSchema),uploadImg,blogsCltr.create);
+// blogRouter.route("/deleteBlog/:id")
+//     .delete(blogsCltr.delete);
+// blogRouter.route("/getBlogs/:id")
+//     .get(blogsCltr.getBlogs);
+// // blogRouter.route("/getBlogs/:counsleorId")
+// //     .get()
+// module.exports=blogRouter   
 const express=require("express")
 const blogsCltr = require("../controllers/blogsCltr")
 const uploadImg = require("../middlewears/uploadImg")
 const blogRouter=express.Router()
 const {checkSchema}=require("express-validator")
 const blogValidationSchema = require("../helpers/blog-validation")
+const authenticate = require("../middlewears/authenticate")
+const checkRole = require("../middlewears/checkRole")
 
 blogRouter.route("/createBlog")
-    .post(checkSchema(blogValidationSchema),uploadImg,blogsCltr.create);
+    .post(authenticate,checkRole(["Counselor","Admin"]),checkSchema(blogValidationSchema),uploadImg,blogsCltr.create);
 blogRouter.route("/deleteBlog/:id")
-    .delete(blogsCltr.delete);
+    .delete(authenticate,checkRole(["Counselor","Admin"]),blogsCltr.delete);
 blogRouter.route("/getBlogs/:id")
-    .get(blogsCltr.getBlogs);
+    .get(authenticate,checkRole(["Counselor"]),blogsCltr.getBlogs);
 // blogRouter.route("/getBlogs/:counsleorId")
 //     .get()
 module.exports=blogRouter   
--- Doc-App-BackEnd/routes/CategoryRoutes.js
+++ Doc-App-BackEnd/routes/CategoryRoutes.js
@@ -2,25 +2,26 @@ const express = require("express");
 const categoryRoutes = express.Router();
 const uploadImg = require("../middlewears/uploadImg");
 const categoryCltr = require("../controllers/categoryCltr");
+const checkRole=require("../middlewears/checkRole")
 
 // Route to create a new category (POST)
 categoryRoutes.route("/createCategory")
-    .post(uploadImg, categoryCltr.create);
+    .post(checkRole(["Admin"]),uploadImg, categoryCltr.create);
 
 // Route to get all categories (GET)
 categoryRoutes.route("/getCategories")
-    .get(categoryCltr.getCategories);
+    .get(checkRole(["Admin"]),categoryCltr.getCategories);
 
 // Route to delete a category by ID (DELETE)
 categoryRoutes.route("/deleteCategory/:categoryId")
-    .delete(categoryCltr.deleteCategory);
+    .delete(checkRole(["Admin"]),categoryCltr.deleteCategory);
 
 // Route to update a category by ID (PUT)
 categoryRoutes.route("/updateCat/:id")
-    .put(categoryCltr.updateCategory);
+    .put(checkRole(["Admin"]),categoryCltr.updateCategory);
 
 // Route to update category image by ID (PATCH)
 categoryRoutes.route("/updateImg/:id")
-    .patch(uploadImg, categoryCltr.imageUpdate);
+    .patch(checkRole(["Admin"]),uploadImg, categoryCltr.imageUpdate);
 
 module.exports = categoryRoutes;
